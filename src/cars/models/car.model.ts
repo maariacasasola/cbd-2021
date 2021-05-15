e5a9d4c0dbd62f67c6ac04fe26b47dd7eff1d364
emicolalc <emicolalc@alum.us.es>
2021-05-15 13:16:12 +0200

    Configuro swagger y hago get

---
+++ src/cars/models/car.model.ts
@@ -0,0 +1,28 @@
+import { ApiModel, ApiModelProperty } from 'swagger-express-ts';
+
+@ApiModel({
+    description: 'Car description',
+    name: 'Car',
+})
+export class CarModel {
+    @ApiModelProperty({
+        description: 'Id of car',
+        example: ['123456789', '12345'],
+        required: true,
+    })
+    public _id: string;
+
+    @ApiModelProperty({
+        description: '',
+        required: true,
+    })
+    public name: string;
+
+    @ApiModelProperty({
+        description: 'Description of car',
+        required: true,
+    })
+    public description: string;
+
+    public _rev: string;
+}
